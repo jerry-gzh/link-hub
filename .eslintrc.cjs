fc24bd7a7173e0c29279c16f1c65e5c5b0553216
Gerardo Andrés Guzmán Hernández <jerry@MacBook-Air-de-Gerardo.local>
2025-11-15 13:44:57 -0600

    Check

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,20 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
     root: true,
-    env: { browser: true, es2021: true },
+
     extends: [
-        "eslint:recommended",
         "plugin:astro/recommended",
-        "plugin:jsx-a11y/recommended"
     ],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module"
-    },
+
     overrides: [
         {
             files: ["**/*.astro"],
             parser: "astro-eslint-parser",
             parserOptions: {
-                parser: "@typescript-eslint/parser",
-                extraFileExtensions: [".astro"]
-            }
-        }
-    ]
+                extraFileExtensions: [".astro"],
+            },
+        },
+    ],
 };
+
+
