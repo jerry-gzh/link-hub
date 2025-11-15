d7a2f831c8f7dab064c8906c9503091c5c288ec5
Gerardo Andrés Guzmán Hernández <jerry@MacBook-Air-de-Gerardo.local>
2025-11-15 01:46:16 -0600

    Adding linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2021: true },
+    extends: [
+        "eslint:recommended",
+        "plugin:astro/recommended",
+        "plugin:jsx-a11y/recommended"
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    overrides: [
+        {
+            files: ["**/*.astro"],
+            parser: "astro-eslint-parser",
+            parserOptions: {
+                parser: "@typescript-eslint/parser",
+                extraFileExtensions: [".astro"]
+            }
+        }
+    ]
+};
